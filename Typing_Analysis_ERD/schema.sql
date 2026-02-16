cb2abd29b09225ab471acb2a142cc9e8505c18aa
V0idStatic <coutforvoid@gmail.com>
2026-02-16 15:51:45 +0800

    Zomtype | Add Constraint

--- Typing_Analysis_ERD/schema.sql
+++ Typing_Analysis_ERD/schema.sql
@@ -7,15 +7,18 @@ CREATE TABLE auth_id
 CREATE TABLE user_level
 (
  user_level_id SERIAL PRIMARY KEY,
- user_id INT REFERENCES auth_id(user_id),
- level_id INT REFERENCES level(level_id)
+ user_id INT,
+ level_id INT,
+ CONSTRAINT fk_auth_user_id FOREIGN KEY (user_id) REFERENCES auth_id(user_id),
+ CONSTRAINT fk_lvl_level_id FOREIGN KEY (level_id) REFERENCES level(level_id)
 );
 
 CREATE TABLE level
 (
  level_id SERIAL PRIMARY KEY,
- level_number INT REFERENCES zombie_typing_game(level_number),
- level_name VARCHAR(100)
+ level_number INT,
+ level_name VARCHAR(100),
+ CONSTRAINT fk_ztg_level_number FOREIGN KEY (level_number) REFERENCES zombie_typing_game(level_number)
  );
 
 
@@ -29,73 +32,88 @@ CREATE TABLE level
 CREATE TABLE zombie_typing_game
 (
  zombie_typing_game_id SERIAL PRIMARY KEY,
- gamemode_id INT REFERENCES gamemode(gamemode_id),
+ gamemode_id INT,
  level_number int,
  sentence TEXT[],
- difficulty Varchar(50)
+ difficulty Varchar(50),
+ CONSTRAINT fk_gm_gamemode_id FOREIGN KEY (gamemode_id) REFERENCES gamemode(gamemode_id)
 );
   
 CREATE TABLE only_typing_game
 (
  only_typing_game_id SERIAL PRIMARY KEY,
- gamemode_id INT REFERENCES gamemode(gamemode_id),
+ gamemode_id INT,
  sentence TEXT[],
- difficulty TEXT[]
-
+ difficulty TEXT[],
+ CONSTRAINT fk_gm_gamemode_id_otg FOREIGN KEY (gamemode_id) REFERENCES gamemode(gamemode_id)
 );
 
 CREATE TABLE session
 (
  session_id SERIAL PRIMARY KEY,
- user_id INT REFERENCES auth_id(user_id),
- gamemode_id INT REFERENCES gamemode(gamemode_id),
- gamep_id INT REFERENCES game_performance(gamep_id),
- common_mistake_id INT REFERENCES common_mistake(common_mistake_id),
- keystroke_id INT REFERENCES keystroke(keystroke_id),
- performance_id INT REFERENCES performance_metrics(performance_id)
+ user_id INT,
+ gamemode_id INT,
+ gamep_id INT,
+ common_mistake_id INT,
+ keystroke_id INT,
+ performance_id INT,
+ CONSTRAINT fk_auth_user_id_sess FOREIGN KEY (user_id) REFERENCES auth_id(user_id),
+ CONSTRAINT fk_gm_gamemode_id_sess FOREIGN KEY (gamemode_id) REFERENCES gamemode(gamemode_id),
+ CONSTRAINT fk_gp_gamep_id FOREIGN KEY (gamep_id) REFERENCES game_performance(gamep_id),
+ CONSTRAINT fk_cm_common_mistake_id FOREIGN KEY (common_mistake_id) REFERENCES common_mistake(common_mistake_id),
+ CONSTRAINT fk_ks_keystroke_id FOREIGN KEY (keystroke_id) REFERENCES keystroke(keystroke_id),
+ CONSTRAINT fk_pm_performance_id FOREIGN KEY (performance_id) REFERENCES performance_metrics(performance_id)
 );
 
 CREATE TABLE game_performance
 (
     gamep_id SERIAL PRIMARY KEY,
-    session_id INT REFERENCES session(session_id),
-    user_id INT REFERENCES session(user_id),
+    session_id INT,
+    user_id INT,
     final_score INT,
     zombies_defeated INT,
-    avg_points DOUBLE PRECISION
+    avg_points DOUBLE PRECISION,
+    CONSTRAINT fk_sess_session_id_gp FOREIGN KEY (session_id) REFERENCES session(session_id),
+    CONSTRAINT fk_sess_user_id_gp FOREIGN KEY (user_id) REFERENCES session(user_id)
 );
 
 CREATE TABLE common_mistake
 (
  common_mistake_id SERIAL PRIMARY KEY,
- session_id INT REFERENCES session(session_id),
- user_id INT REFERENCES session(user_id),
+ session_id INT,
+ user_id INT,
  correct INT,
- incorrect INT
+ incorrect INT,
+ CONSTRAINT fk_sess_session_id_cm FOREIGN KEY (session_id) REFERENCES session(session_id),
+ CONSTRAINT fk_sess_user_id_cm FOREIGN KEY (user_id) REFERENCES session(user_id)
 );
 
 CREATE TABLE keystroke
 (
  keystroke_id SERIAL PRIMARY KEY,
- session_id INT REFERENCES session(session_id),
- user_id INT REFERENCES session(user_id),
- total_keystroke INT
+ session_id INT,
+ user_id INT,
+ total_keystroke INT,
+ CONSTRAINT fk_sess_session_id_ks FOREIGN KEY (session_id) REFERENCES session(session_id),
+ CONSTRAINT fk_sess_user_id_ks FOREIGN KEY (user_id) REFERENCES session(user_id)
 );
 
 CREATE TABLE performance_metrics
 (
  performance_id SERIAL PRIMARY KEY,
- session_id INT REFERENCES session(session_id),
- user_id INT REFERENCES session(user_id),
+ session_id INT,
+ user_id INT,
  wpm INT,
  cpm INT,
  accuracy DOUBLE PRECISION,
- duration TIMESTAMP
+ duration TIMESTAMP,
+ CONSTRAINT fk_sess_session_id_pm FOREIGN KEY (session_id) REFERENCES session(session_id),
+ CONSTRAINT fk_sess_user_id_pm FOREIGN KEY (user_id) REFERENCES session(user_id)
 );
 
 CREATE TABLE leaderboard
 (
  leaderboard_id SERIAL PRIMARY KEY,
- user_id INT REFERENCES session(user_id)
-
+ user_id INT,
+ CONSTRAINT fk_sess_user_id_lb FOREIGN KEY (user_id) REFERENCES session(user_id)
 );
